0d6c515a8b432cc032481d29ed8e06112d67ba5b
kerdamon <kwalas314@gmail.com>
2025-09-22 00:37:46 +0200

    chore(config): add typescript

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import express from "express";
+import express, { type Request, type Response } from "express";
 const app = express();
 const port = "3000";
 
-app.get("/", (req, res) => {
-  res.send("Hello cruel asdf World!");
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
   console.log("Response sent");
 });
 
